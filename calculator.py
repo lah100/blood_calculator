74183bd458399bb22d14111cceb212adf7136608
unknown <lah100@duke.edu>
2020-08-24 12:38:57 -0400

    Develop initial user interface including quit option

---
+++ calculator.py
@@ -0,0 +1,10 @@
+def interface():
+    print("My Program")
+    print("Option:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice == '9':
+        return
+
+
+interface()
